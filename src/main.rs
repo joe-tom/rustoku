11253138f332f058878e2de685100a81ffdb0b2a
Joseph Thomas <joey6978@gmail.com>
2016-12-25 17:44:46 -0800

    Going back and forth between binary and ternary. I'm going to build everything without diagonal moves first.

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![feature(test)]
+#![feature(type_ascription)]
+
+extern crate test;
+use test::Bencher;
+
+mod board;
+
+
+
+#[bench]
+fn gen_moves(b: &mut test::Bencher) {
+  let mut brd = board::Board {
+    multi: [[0; 15]; 15],
+    
+    horiz_y: [0; 15],
+    horiz_o: [0; 15],
+    verti_y: [0; 15],
+    verti_o: [0; 15],
+    diagr_y: [0; 19],
+    diagr_o: [0; 19],
+    diagl_y: [0; 19],
+    diagl_o: [0; 19]
+  };
+  
+  b.iter(|| {
+    let a = test::black_box(10);
+    let b = test::black_box(false);
+    brd.place_piece(a,b);
+  })
+}
